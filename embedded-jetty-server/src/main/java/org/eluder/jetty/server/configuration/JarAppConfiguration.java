f5d405f09af89f922fff5e690170592851e61055
trautonen <trautonen@github.com>
2013-12-29 00:23:21 +0200

    Logging.

--- embedded-jetty-server/src/main/java/org/eluder/jetty/server/configuration/JarAppConfiguration.java
+++ embedded-jetty-server/src/main/java/org/eluder/jetty/server/configuration/JarAppConfiguration.java
@@ -1,5 +1,7 @@
 package org.eluder.jetty.server.configuration;
 
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.webapp.WebInfConfiguration;
 import org.eluder.jetty.server.ServerConfig;
@@ -10,6 +12,8 @@ import org.eluder.jetty.server.ServerConfig;
  */
 public class JarAppConfiguration extends WebInfConfiguration {
 
+    private static final Logger LOG = Log.getLogger(JarAppConfiguration.class);
+    
     @Override
     public void preConfigure(final WebAppContext context) throws Exception {
         if (isJar(context.getWar())) {
@@ -20,6 +24,7 @@ public class JarAppConfiguration extends WebInfConfiguration {
                 extraClasspath = context.getWar() + "," + extraClasspath;
             }
             context.setExtraClasspath(extraClasspath);
+            LOG.debug("Set extra classpath to: {}", extraClasspath);
         }
         super.preConfigure(context);
     }
